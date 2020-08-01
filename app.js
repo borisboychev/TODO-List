896f824f2a2133331f86ff1e8db682742e063dcc
BorisBBoychev <borisboychev007@Gmail.com>
2020-08-01 15:38:59 +0300

    check and trash button functionality

--- app.js
+++ app.js
@@ -11,10 +11,23 @@ function addTODO(event) {
   //todo div
   const todoDiv = document.createElement("div");
   todoDiv.classList.add("todo");
-
   const newTodo = document.createElement("li");
   newTodo.innerText = "hey";
   newTodo.classList.add("todo-item");
-
   todoDiv.appendChild(newTodo);
+
+  //check button
+  const checkedButton = document.createElement("button");
+  checkedButton.innerHTML = '<i class="fas fa-check"></i>';
+  checkedButton.classList.add("checked-button");
+  todoDiv.appendChild(checkedButton);
+
+  //trash button
+  const trashButton = document.createElement("button");
+  trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+  trashButton.classList.add("trash-button");
+  todoDiv.appendChild(trashButton);
+
+  //append to list
+  todoList.appendChild(todoDiv);
 }
